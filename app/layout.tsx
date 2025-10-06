a8561dead0cc4fb9a38c636d4afb1b34af903cf8
Derky <alex.miguel.1805@gmail.com>
2025-10-06 09:25:18 +0200

    Added Analytics & SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AnimationProvider } from "@/components/providers/AnimationProvider";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +33,8 @@ export default function RootLayout({
         <AnimationProvider>
           {children}
         </AnimationProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
